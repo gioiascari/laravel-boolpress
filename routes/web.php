c806dcc2fe12aaf9ea52300c62d819dcd87d12c3
gioiascari <gioia.ascari96@gmail.com>
2022-05-30 16:44:36 +0200

    add guest.home, admin.home e collegato la route di guest

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+//Rotte per l'autenticazione
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+// Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function (){
+        Route::get('/', 'HomeController@index')
+        ->name('home');
+    });
+//Si utilizza per ultima perché prima deve controllare le rotte precedenti e per ultima questa sotto
+//'any?' significa che prende qualsiasi percorso
+Route::get('{any?}', function(){
+    return view('guest.home');
+})->where("any", ".*"); //L'ultimo simbolo tra apici richiama qualsiasi file
