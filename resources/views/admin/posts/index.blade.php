98f8f9343469b4952e90aef3ea3b53fef4d1bbec
gioiascari <gioia.ascari96@gmail.com>
2022-05-31 19:49:40 +0200

    fixed bug on index.blade

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -3,23 +3,27 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-between align-items-center">
-            <div class="col-12">
-                <div class="d-flex justify-content-between align-items-center">
-                    <h1>Tutti i posts</h1>
-                    <a href="{{ route('admin.posts.create') }}" class="btn btn-primary">
-                    Crea nuovo post
-                    </a>
+            <div class="col-12 ">
+                <div class="allPosts d-flex justify-content-between align-items-center">
+                    <h1>All Posts</h1>
+                    <a href="{{route('admin.posts.create')}}" class="btn btn-info"> Add new Post</a>
+
                 </div>
+                {{-- Creo una tabella per mettere gli attributi al di sotto dei titoli adatti, so che non bisognerebbe farla ma non mi viene in mente nient'altro  --}}
+
                 <table class="table">
                     <thead>
+                        {{-- Table title  --}}
                         <tr>
                             <th>ID</th>
-                            <th>Titolo</th>
+                            <th>Title</th>
                             <th>Slug</th>
-                            <th class="text-center">Azioni</th>
+                            <th class="text-center">Actions</th>
                         </tr>
+                        {{--/ Table title  --}}
                     </thead>
                     <tbody>
+                        {{-- Table content  --}}
                         @foreach ($posts as $post)
                             <tr>
                                 <td>{{ $post->id }}</td>
@@ -31,8 +35,13 @@
                                 </td>
                             </tr>
                         @endforeach
+                        {{--/ Table content  --}}
+
                     </tbody>
                 </table>
             </div>
         </div>
     </div>
+@endsection
+
+
