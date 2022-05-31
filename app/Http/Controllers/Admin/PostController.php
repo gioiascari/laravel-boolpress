37c1dae77b8b6340b589f51e94dac7a6f211d3e9
gioiascari <gioia.ascari96@gmail.com>
2022-05-31 19:19:33 +0200

    add create.blade successfully

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Post;
 class PostController extends Controller
