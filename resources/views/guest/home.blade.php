02d9395aff7dc8456af853cb42811495129e948a
gioiascari <gioia.ascari96@gmail.com>
2022-06-01 22:17:34 +0200

    add some style to guest, ex done :)

--- resources/views/guest/home.blade.php
+++ resources/views/guest/home.blade.php
@@ -1,2 +1,18 @@
-<a href="{{route('login')}}">login</a>
-<a href="{{route('register')}}">register</a>
+@extends('layouts.app')
+@section('content')
+<div class="container">
+    <div class="row align-items-center text-center">
+        <div class="col-12">
+            <h1>You are not logged</h1>
+            <div class="d-grid gap-4 align-items-center">
+                <a href="{{route('register')}}" class="btn btn-primary color-white">Get Started</a>
+                <a href="{{route('login')}}" class="btn btn-primary color-white">Login</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@endsection
+
+
