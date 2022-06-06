8eb10c88bc691036f1628f584c4ee3a8687a989c
gioiascari <gioia.ascari96@gmail.com>
2022-06-06 15:32:47 +0200

    add belongsToMany

---
+++ app/Tags.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tags extends Model
+{
+    protected $table = 'tags';
+
+    public function posts(){
+        return $this->belongsToMany('App\Post');
+    }
+}
