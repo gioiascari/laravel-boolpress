db4b1e704316c662f296e88547553b64bd1e72a6
gioiascari <gioia.ascari96@gmail.com>
2022-06-06 18:31:24 +0200

    add function tags to post model

--- app/Post.php
+++ app/Post.php
@@ -9,7 +9,9 @@ class Post extends Model
     protected $fillable = ['title', 'content', 'slug', 'category_id'];
     public function category(){
         return $this->belongsTo('App\Category');
-
-
+    }
+    public function tags()
+    {
+        return $this->belongsToMany("App\Tags");
     }
 }
